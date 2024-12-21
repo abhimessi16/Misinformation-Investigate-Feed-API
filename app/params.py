3689c17e3e6bfcd5b8d362287f95eef9c8fc120c
abhimessi16 <piabhi18@gmail.com>
2024-12-21 20:50:59 +0530

    chore: load env variables from env file

--- app/params.py
+++ app/params.py
@@ -1,4 +1,10 @@
-kafka_bootstrap_servers = ['13.233.136.176:9092']
+import os
+from dotenv import load_dotenv
+
+if not load_dotenv():
+    print("Environment variables not loaded!")
+
+kafka_bootstrap_servers = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
 kafka_topics = {
     "lv": "liveFeed"
 }
