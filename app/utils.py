38c42662c7394aeb04cc0151e2896154b7725e68
abhimessi16 <piabhi18@gmail.com>
2024-12-30 14:22:18 +0530

    feature: producer send update, chore: requirements update

--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,17 @@
 import subprocess
 import os, signal
 
+import numpy as np
+
 from app.models import LiveStream
 from app.params import kafka_topics
 import app.main as session
 
 def get_audio_from_m3u8_send_to_producer(m3u8_data: LiveStream, pid_store: dict):
     # add a retry mechanism, check how?
+    print(m3u8_data.m3u8_link)
     ffmpeg_process = subprocess.Popen(
-        ['ffmpeg', '-loglevel', 'quiet', '-i', m3u8_data.m3u8_link, '-t', '1', '-f', 'wav', 'pipe:1'], # duration of 1 for now, add ac -1 later
+        ['ffmpeg', '-loglevel', 'quiet', '-vn', '-i', m3u8_data.m3u8_link, '-t', '180','-acodec', 'pcm_s16le','-ac', '1', '-ar', '16000', '-f', 'wav', 'pipe:1'], # duration of 180 for now
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
@@ -28,7 +31,7 @@ def send_audio_data_to_kafka(audio_data: bytes, m3u8_data: LiveStream):
     
     produced_data = {
         "source": m3u8_data.source,
-        "audio_data": audio_data.decode("utf-16le"),
+        "audio_data": np.frombuffer(audio_data, dtype=np.int16).tolist(),
         "session_id": m3u8_data.session_id
     }
     try:
@@ -37,14 +40,12 @@ def send_audio_data_to_kafka(audio_data: bytes, m3u8_data: LiveStream):
             # key=m3u8_data.source, # gonna implement this later
             value=produced_data
         )
-        # waiting for the data to be sent is not the best idea,
-        # it kinda beats the purpose of real-time syncing of data
     except Exception as ex:
         # what can we do if an exception arises
         print(ex)
-    finally:
-        # what can we do finally
-        print(f"sent data: {len(audio_data)}.")
+    # finally:
+    #     # what can we do finally
+    #     print(f"sent data: {len(audio_data)}.")
 
 def kill_subprocess(pid: int):
     os.kill(pid, signal.SIGTERM)
